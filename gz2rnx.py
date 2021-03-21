f1c43277d2a8c3da35ca0e0b47d8ed4de41749cc
Alex <1321312087@qq.com>
2021-03-21 09:42:10 +0800

    unzip gz-file

--- gz2rnx.py
+++ gz2rnx.py
@@ -4,12 +4,14 @@ import gzip
 
 
 def init():
-    allFile = os.listdir("./resource")
+    global relativePath
+    relativePath = "./resources"
+    allFile = os.listdir(relativePath)
     for i in range(len(allFile)):
         # 只通过文件
-        if os.path.isfile(allFile[i]):
+        if os.path.isfile(relativePath+"/"+allFile[i]):
             # 只处理.gz文件
-            if allFile[i].split(".")[-1] == "gz":
+            if (allFile[i].split(".")[-1] == "gz"):
                 gzName = allFile[i]
                 unzip(gzName)
 
@@ -18,11 +20,14 @@ def unzip(filename):
     # 提取出年份
     year = filename.split("_")[2][2:4]
     # 获取文件的名称，并转换为D文件（.gz->.D）
-    unzipFile = filename.replace(".gz", "." + year + "D")
+    if filename.split(".")[-2] == "crx":
+        unzipFile = filename.replace("crx.gz", year + "D")
+    elif filename.split(".")[-2] == "rnx":
+        unzipFile = filename.replace(".gz", "")
     # 创建gzip对象
-    gzFile = gzip.GzipFile(filename)
+    gzFile = gzip.GzipFile(relativePath+"/"+filename)
     # gzip对象用read()打开后，写入open()建立的文件里
-    open(unzipFile, "wb+").write(gzFile.read())
+    open(relativePath+"/"+unzipFile, "wb+").write(gzFile.read())
     # 关闭gzip对象
     gzFile.close()
 
@@ -31,6 +36,3 @@ if __name__ == "__main__":
     print("gz文件开始解压......")
     init()
     print("gz文件解压完成！")
-    print("D文件向O文件转换......")
-    os.system("批量转换")
-    print("转换完成")
